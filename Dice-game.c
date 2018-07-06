952f3c7082dc8d0913e2c3cb228d8de65c2ee045
s1250125 <s1250125@std1dc21.u-aizu.ac.jp>
2018-07-07 04:27:30 +0900

    Won/lost message added

--- Dice-game.c
+++ Dice-game.c
@@ -9,10 +9,14 @@ int main(){
  
   s1 = rand() % 6 + 1;
   s2 = rand() % 6 + 1;
- 
+
+  printf("Rolling the dice...\n");
   printf("Die 1: %d\nDie 2: %d\n",s1,s2);
   printf("Total value: %d\n",s1+s2);
- 
+
+  if((s1+s2) > 7) printf("You won!\n");
+  else printf("You lost!\n");
+  
   return 0;
 }
 
